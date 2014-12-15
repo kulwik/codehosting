f895fdb260ec06f693f70f72c9f17f851224c25c
Marcin <mkulwikowski@ibt.pt>
2014-12-15 15:41:04 +0000

    gitManager

--- getHello.js
+++ getHello.js
@@ -1,3 +1,13 @@
-function Hello(res){
-	res.send(200, "Hello...");
+function Hello(res, modules){
+    var items = [];
+    modules.storageRef.table("todoTable").getItems(function(itemSnapshot) {
+        if (itemSnapshot) {
+            items.push(itemSnapshot.val());
+        } else {
+            res.send(200, items);
+        }
+    },
+    function(error) {
+        res.send(400, error);
+    });
 }
