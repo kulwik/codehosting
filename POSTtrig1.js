d1fee5fbc53602f46c47c7801291dfb8707f6420
Marcin <mkulwikowski@ibt.pt>
2014-12-26 18:18:51 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -1,5 +1,5 @@
 function trig1(request,res){
     console.log('z trig: ' + JSON.stringify(request.body));
     request.body.item.message = request.body.item.message.toUpperCase();
-	res.send(200, request.body.item);
+	res.send(200/*, request.body.item*/);
 }
