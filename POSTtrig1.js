0b1bdcbb49ec4ce88e0e443e33ee76f900545943
Marcin <mkulwikowski@ibt.pt>
2015-01-02 16:31:53 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -1,6 +1,11 @@
 function trig1(request,res){
     console.log('z trig: ' + JSON.stringify(request.body));
     console.log('scope: ' + this);
+    for (var key in this) {
+      if (this.hasOwnProperty(key)) {
+        console.log(key + " -> " + this[key]);
+      }
+    }
     //request.body.item.message = request.body.item.message.toUpperCase();
 	res.send(400/*{'dupa': 'not in my house!'}, request.body.item*/);
 }
