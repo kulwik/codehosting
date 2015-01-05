d4093f43d540246a19d922ce660aabb17a10b399
Marcin <mkulwikowski@ibt.pt>
2015-01-05 11:02:14 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -1,6 +1,6 @@
 function trig1(request,res){
     console.log('z trig: ' + JSON.stringify(request.body));
-    console.log('scope: ' + triggerNestLevel);
+    //console.log('scope: ' + triggerNestLevel);
     for (var key in this) {
       if (this.hasOwnProperty(key)) {
         console.log(key + " -> " + this[key]);
