90bd12fdc3f8875566b81bd6a4534717e87566f7
Marcin <mkulwikowski@ibt.pt>
2015-01-12 13:21:40 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -2,11 +2,11 @@ function trig1(request,res, modules){
     console.log('z trig: ' + JSON.stringify(request.body));
     //console.log('scope: ' + triggerNestLevel);
     var item = request.body.item;
-    item.message = 'from trig';
+    item.message = item.message + '*';
     modules.storageRef.table("todoTable").push(
         item,
         function (itemSnapshot){
-            console.log('item push success');
+            console.log('item push success: ' + JSON.stringify(itemSnapshot.val()));
         },
         function (error){
             console.log('error: ' + error);
