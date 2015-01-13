fb86a1d04556991e4eefc5c477d27758330f0199
Marcin <mkulwikowski@ibt.pt>
2015-01-13 13:50:17 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -4,7 +4,7 @@ function trig1(request,res, modules){
     var item = request.body.item;
     item.message = item.message + 'x';
     
-    res.send(400);
+    res.send(400, 'wola');
     //res.send(200, {item: item});
     
     /*
