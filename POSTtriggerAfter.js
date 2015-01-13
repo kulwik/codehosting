2c68023bc41a0d42eb0e1a6f603147584ad4ef83
Marcin <mkulwikowski@ibt.pt>
2015-01-13 11:49:33 +0000

    gitManager

--- POSTtriggerAfter.js
+++ POSTtriggerAfter.js
@@ -1,7 +1,9 @@
 function triggerAfter(res,req,modules){
-    modules.storageRef.table('todoTable').item(req.params.key).incr('state', 1, function(){
+    modules.storageRef.table('todoTable').item(req.params.key).incr('state', 1, function(is){
+        console.log('ok: ' + JSON.stringify(is));
         res.send(200);    
     }, function(error){
+        console.log('error: ' + error);
         res.send(400, error);
     });
 	
