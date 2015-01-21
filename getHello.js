7c1c0f9a14b17ffbc1ddd5ca4a4520b06f14dd15
root <root@ip-10-136-52-23.ec2.internal>
2015-01-21 11:59:28 +0000

    gitManager

--- getHello.js
+++ getHello.js
@@ -1,13 +1,14 @@
 function Hello(res, modules){
     var items = [];
     modules.storageRef.table("todoTable").getItems(function(itemSnapshot) {
-        if (itemSnapshot) {
-            items.push(itemSnapshot.val());
-        } else {
-            res.send(200, items);
+            if (itemSnapshot) {
+                items.push(itemSnapshot.val());
+            } else {
+                res.send(200, items);
+            }
+        },
+        function(error) {
+            res.send(400, error);
         }
-    },
-    function(error) {
-        res.send(400, error);
-    });
+    );
 }
