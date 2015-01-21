6df695d6089553883437efd23d13e1254c2269cd
root <root@ip-10-136-52-23.ec2.internal>
2015-01-21 18:37:56 +0000

    gitManager

--- GETtest.js
+++ GETtest.js
@@ -2,7 +2,7 @@ function test(res, modules){
     modules.storageRef.table("todoTable").getItem({ primary: "resttest", secondary: 1378830551 },
     
     function(itemSnapshot) {
-        res.send(200);
+        res.send(200, 'cq');
     },
     function(error) {
         res.send(400, error);
