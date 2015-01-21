72c07b41948c0422e51913999d8a6fe0858e3520
root <root@ip-10-136-52-23.ec2.internal>
2015-01-21 18:49:34 +0000

    gitManager

--- GETtest.js
+++ GETtest.js
@@ -1,5 +1,5 @@
 function test(res, modules){
-    console.log("test");
+    console.log("test " + JSON.stingify(modules.storageRef));
     modules.storageRef.table("todoTable").item({ primary: "resttest", secondary: 1378830551 },
     
     function(itemSnapshot) {
