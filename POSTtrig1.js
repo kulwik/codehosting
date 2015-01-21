6e34510187cb1224e63ecaa316a0f70ab10e1e9a
root <root@ip-10-136-52-23.ec2.internal>
2015-01-21 17:52:27 +0000

    gitManager

--- POSTtrig1.js
+++ POSTtrig1.js
@@ -1,9 +1,13 @@
 function trig1(request,res, modules){
-    console.log('Trigger data: ' + JSON.stringify(request.body));
+    res.send(200);
+    
+    //console.log('Trigger data: ' + JSON.stringify(request.body));
     //console.log('scope: ' + triggerNestLevel);
+    
+    /*
     var item = request.body.item;
     item.message = item.message + 'x';
-    
+    */
     //res.send(400);
     //res.send(200, {item: item});
     
@@ -15,7 +19,7 @@ function trig1(request,res, modules){
     }, 50);*/
 
     //res.send(200, {item:item});
-    
+    /*
     modules.storageRef.table("todoTable").push(
         item,
         function (itemSnapshot){
@@ -26,7 +30,7 @@ function trig1(request,res, modules){
             console.log('error: ' + error);
             res.send(400);
         }
-    );
+    );*/
     
     //request.body.item.message = request.body.item.message.toUpperCase();
 	
